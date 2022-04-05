94e50bb4511dcb40d06ebc51ebdf45f6e315063b
Laurent Domenech-Cabaud <laurent@confluent.io>
2022-04-05 13:46:48 +0200

    v1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import re
 
 from recap import get_recap_table
 from squid import get_latest_table, map_table
@@ -15,26 +16,71 @@ def models():
     return jsonify(model_names)
 
 
+def requested_models(request, models_map):
+    if request.json and request.json.get("models"):
+        return request.json["models"]
+
+    if request.form:
+        return [models_map[k] for k in request.form.keys()]
+
+    return []
+
+
+def normalized_model_name(model_name):
+    result = model_name.lower()
+    result = re.sub(r'[\s]', '-', result)
+    result = re.sub(r'[é]', 'e', result)
+    result = re.sub(r'[^a-z-\d]', '', result)
+    return result
+
+
 def get_data():
     table = get_latest_table()
     timing_map = map_table(table)
 
-    try:
-        requested_models = request.json["models"]
-        print(requested_models)
+    models_map = {normalized_model_name(m): m for m in timing_map.keys()}
 
-        if requested_models:
-            filtered_map = {}
-            for model, data in timing_map.items():
-                if model in requested_models:
-                    filtered_map[model] = data
-            timing_map = filtered_map
-    except:
-        pass
+    models = requested_models(request, models_map)
+    if models:
+        filtered_map = {}
+        for model, data in timing_map.items():
+            if model in models:
+                filtered_map[model] = data
+        timing_map = filtered_map
 
     recap_table = get_recap_table(timing_map)
 
     return timing_map, recap_table
+
+
+
+
+def build_model_form(models):
+
+    model_list = "\n".join([f"<input type='checkbox' name='{normalized_model_name(m)}'></input><label for='{normalized_model_name(m)}'>{m}</label><br />" for m in models])
+
+    form = f"<form id='models' action'/html' method='POST' enctype='application/x-www-form-urlencoded'>{model_list}" \
+           f"<button type='submit' form='models' value='view' formaction='/html'>View</button>" \
+           f"</form>"
+
+    return f"""
+    <!doctype html>
+    <html lang="fr">
+    <head>
+      <meta charset="utf-8">
+      <title>Models Availability</title>
+      <style>
+      body {{
+        font-family: "arial";
+        font-size: small;
+      }}
+      </style>
+    </head>
+    <body>
+        {form}    
+    </body>
+    </html>
+        """
 
 
 def build_html(recap_table):
@@ -54,6 +100,31 @@ def build_html(recap_table):
 <head>
   <meta charset="utf-8">
   <title>Models Availability</title>
+  <style>
+  th {{
+    background-color: #333;
+    color: #eee;
+    padding: 3px;
+  }}
+  body {{
+    font-family: "arial";
+    font-size: small;
+    text-align: center;
+  }}
+  table {{
+    display: table;
+    border: 1px solid #333;
+    border-spacing: 0;
+    -webkit-border-horizontal-spacing: 0;
+    -webkit-border-vertical-spacing: 0;
+  }}
+  td {{ 
+    border-top: 1px solid #666;
+    border-right: 1px dotted #666;
+  }}
+  tr:nth-child(even) {{background: #CCC}}
+  tr:nth-child(odd) {{background: #FFF}}
+  </style>
 </head>
 <body>
     {html_table}    
@@ -65,8 +136,14 @@ def build_html(recap_table):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     timing_map, recap_table = get_data()
-    return jsonify({"timing_details": timing_map, "recap_table": recap_table})
 
+    html_result = build_model_form(timing_map.keys())
+    return html_result
+
+@app.route('/json', methods=['POST', 'GET'])
+def json():
+    timing_map, recap_table = get_data()
+    return jsonify({"timing_details": timing_map, "recap_table": recap_table})
 
 @app.route('/html', methods=['POST', 'GET'])
 def html():
