1708d91ffe5cb92a915516cfbb1eb9b71914cb12
Laurent Domenech-Cabaud <laurent@confluent.io>
2022-04-06 07:11:38 +0200

    template + other improvements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import re
 
 from recap import get_recap_table
@@ -53,38 +53,21 @@ def get_data():
     return timing_map, recap_table
 
 
+def model_checkboxes(models):
+    return [f"<div class='item'><input type='checkbox' name='{normalized_model_name(m)}' " \
+            f"id='{normalized_model_name(m)}'></input>"
+            f"<label for='{normalized_model_name(m)}'>{m}</label></div>"
+            for m in models]
 
 
-def build_model_form(models):
-
-    model_list = "\n".join([f"<input type='checkbox' name='{normalized_model_name(m)}'></input><label for='{normalized_model_name(m)}'>{m}</label><br />" for m in models])
-
-    form = f"<form id='models' action'/html' method='POST' enctype='application/x-www-form-urlencoded'>{model_list}" \
-           f"<button type='submit' form='models' value='view' formaction='/html'>View/Voir</button>" \
-           f"</form>"
-
-    return f"""
-    <!doctype html>
-    <html lang="fr">
-    <head>
-      <meta charset="utf-8">
-      <title>Models Availability</title>
-      <style>
-      body {{
-        font-family: "arial";
-        font-size: small;
-      }}
-      </style>
-    </head>
-    <body>
-        <div>Select the models your wish to use - Sélectionnez les modèles que vous voulez utiliser :</div>
-        {form}    
-    </body>
-    </html>
-        """
+def build_flex_form(checkboxes):
+    result = '<div class="container">'
+    result += "\n".join(checkboxes)
+    result += "</div>"
+    return result
 
 
-def build_html(recap_table):
+def build_html_table(recap_table):
     header = ""
     table_body = ""
     for i, row in enumerate(recap_table):
@@ -93,70 +76,29 @@ def build_html(recap_table):
         else:
             table_body += "<tr><td class='model'>" + "</td><td>".join(row) + "</td></tr>"
 
-    html_table = f"<table><thead>{header}</thead><tbody>{table_body}</tbody></table>"
-
-    return f"""
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Models Availability</title>
-  <style>
-  th {{
-    background-color: #333;
-    color: #eee;
-    padding: 3px;
-  }}
-  body {{
-    font-family: "arial";
-    text-align: center;
-  }}
-  table {{
-    display: table;
-    border: 1px solid #333;
-    border-spacing: 0;
-    -webkit-border-horizontal-spacing: 0;
-    -webkit-border-vertical-spacing: 0;
-  }}
-  th, td {{
-    padding: 10px;
-  }}
-  td {{ 
-    border-top: 1px solid #666;
-    border-right: 1px dotted #666;
-  }}
-  td.model {{ 
-    text-align: right;
-  }}
-  tr:nth-child(even) {{background: #CCC}}
-  tr:nth-child(odd) {{background: #FFF}}
-  </style>
-</head>
-<body>
-    {html_table}    
-</body>
-</html>
-    """
+    return f"<table><thead>{header}</thead><tbody>{table_body}</tbody></table>"
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
     timing_map, recap_table = get_data()
+    checkboxes = model_checkboxes(timing_map.keys())
+    model_form = build_flex_form(checkboxes)
 
-    html_result = build_model_form(timing_map.keys())
-    return html_result
+    return render_template('index.html', model_form=model_form)
+
 
 @app.route('/json', methods=['POST', 'GET'])
 def json():
     timing_map, recap_table = get_data()
     return jsonify({"timing_details": timing_map, "recap_table": recap_table})
 
-@app.route('/html', methods=['POST', 'GET'])
+
+@app.route('/', methods=['POST'])
 def html():
     timing_map, recap_table = get_data()
-
-    html_result = build_html(recap_table)
-    return html_result
+    table = build_html_table(recap_table)
+    return render_template('result.html', html_table=table)
 
 
 if __name__ == '__main__':
